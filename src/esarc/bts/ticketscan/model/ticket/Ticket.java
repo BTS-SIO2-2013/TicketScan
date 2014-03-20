eb6d8e1520e9bdd4b40b79f15cee3fbfd5cd313b
Alexis <amaigrot@gmail.com>
2014-03-20 10:52:46 +0100

    Ajout Event

--- src/esarc/bts/ticketscan/model/ticket/Ticket.java
+++ src/esarc/bts/ticketscan/model/ticket/Ticket.java
@@ -1,5 +1,9 @@
 package esarc.bts.ticketscan.model.ticket;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -32,5 +36,19 @@ public class Ticket {
 	public static Ticket loadJson(String json) throws JSONException {
 		JSONObject jsonT = new JSONObject(json);
 		return new Ticket(jsonT.getString("nom"),jsonT.getInt("code"));
+	}
+
+	public static List<Ticket> ticketListFromJSON(String json) throws JSONException {
+		List<Ticket> list = new ArrayList<Ticket>();
+		
+		JSONArray jsonArray = new JSONArray(json);
+		
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject ticketJSON = jsonArray.getJSONObject(i);
+			Ticket ticket = Ticket.loadJson(ticketJSON.toString());
+			list.add(ticket);
+		}
+		
+		return list;
 	}
 }
